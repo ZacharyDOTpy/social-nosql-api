78a2d31ebb59f95528f6199f8d640553096c8a7d
Zach <zach.Brewer.1995@gmail.com>
2024-04-09 19:58:45 -0400

    rewrote userController & reaction model

--- models/reaction.js
+++ models/reaction.js
@@ -1,25 +1,32 @@
-const mongoose = require('mongoose');
+const { Schema, Types } = require('mongoose');
 
-const reactionSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => new Date(createdAtVal).toLocaleDateString(),
+    },
   },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-  thoughtId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Thought',
-    required: true,
-  },
-
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-});
-
-const Reaction = mongoose.model('Reaction', reactionSchema);
-
-module.exports = Reaction;
+module.exports = reactionSchema;
