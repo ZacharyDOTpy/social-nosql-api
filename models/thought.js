83def92ca0b23f2fe5f130506f9bef0862d48ac4
Zach <zach.Brewer.1995@gmail.com>
2024-04-09 20:01:26 -0400

    rewrote thought model

--- models/thought.js
+++ models/thought.js
@@ -1,35 +1,38 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
 
-const thoughtSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-
-  reactions: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Reaction',
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
     },
-  ],
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => {
-      return createdAtVal.toISOString();
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => new Date(createdAtVal).toLocaleString(),
     },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
 });
 
-const Thought = mongoose.model('Thought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
